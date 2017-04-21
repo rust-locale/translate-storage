7d7a7fc812e82e559bd58c3db7d48d6918e14bb1
Jan Hudec <bulb@ucw.cz>
2017-04-21 20:38:47 +0200

    Initial version that builds, but may not work.
    It also does not yet interpret metadata.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,293 @@
+//! Translation catalogues are key part of any localization infrastructure. They contain the lists
+//! of messages from the application, possibly disambiguated with identifiers or contexts, and
+//! corresponding translations.
+//!
+//! Catalogs are usually stored in one of two formats: [Portable Objects (`.po`)][PO], used
+//! primarily by [GNU gettext][gettext], and [XML Localisation Interchange File Format
+//! (XLIFF)][XLIFF], a more generic OASIS open standard.
+//!
+//! These formats can be converted to each other, and to and from many others, using
+//! [translate-toolkit][tt].
+//!
+//! [XLIFF] is quite flexible and can be used in different ways, but this library focuses
+//! primarily on using it in a way [gettext] and [translate-toolkit][tt] work, namely with separate
+//! catalogue for each language.
+//!
+//! [PO]: https://www.gnu.org/software/gettext/manual/html_node/PO-Files.html
+//! [XLIFF]: https://www.oasis-open.org/committees/xliff/
+//! [gettext]: https://www.gnu.org/software/gettext/
+//! [tt]: http://toolkit.translatehouse.org/
+
+#[macro_use]
+extern crate lazy_static;
+
+extern crate regex;
+
+use std::collections::BTreeMap;
+
+// Auxiliary macros for match checking and then not holding on to the value:
+macro_rules! unpack {
+    ($x:expr => $p:pat => $r:expr) => {{
+        match $x {
+            $p => Some($r),
+            _ => None,
+        }
+    }};
+    ($x:expr => $p:pat if $c:expr => $r:expr) => {{
+        match $x {
+            $p if $c => Some($r),
+            _ => None,
+        }
+    }};
+}
+
+macro_rules! is {
+    ($x:expr => $p:pat) => {{
+        match $x {
+            $p => true,
+            _ => false,
+        }
+    }};
+    ($x:expr => $p:pat if $c:expr) => {{
+        match $x {
+            $p if $c => true,
+            _ => false,
+        }
+    }};
+}
+
+
+pub mod po;
+
+/// Plural variants
+///
+/// Which variants are used depends on the language. In English it is easy: 1 is One and everything
+/// else is Other. But other languages may have more cases, with Arabic having all six.
+#[derive(Copy,Clone,Debug,Eq,PartialEq,Ord,PartialOrd,Hash)]
+pub enum Count {
+    /// Zero has a separate variant in some langauges.
+    Zero,
+    /// One. In some languages also includes zero.
+    One,
+    /// Special case for two.
+    Two,
+    /// Small number. What is small number depends on the language.
+    Few,
+    /// Large number. What is large number depends on the language.
+    Many,
+    /// Any other number.
+    Other,
+}
+
+impl Default for Count {
+    fn default() -> Count { Count::One }
+}
+
+/// String wrapper possibly with plural variants.
+///
+/// This is used for source and target strings in translation Unit.
+#[derive(Clone,Debug,Eq,PartialEq,Ord,PartialOrd,Hash)]
+pub enum Message {
+    /// Unset message, used for untranslated entries.
+    Empty,
+    /// Message independent of any count.
+    Singular(String),
+    /// Count-dependent message with some variants. Must have at least variant for Other.
+    Plural(BTreeMap<Count, String>),
+}
+
+impl Message {
+    pub fn is_empty(&self) -> bool {
+        match self {
+            &Message::Empty => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_singular(&self) -> bool {
+        match self {
+            &Message::Singular(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_plural(&self) -> bool {
+        match self {
+            &Message::Plural(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_blank(&self) -> bool {
+        match self {
+            &Message::Empty => true,
+            &Message::Singular(ref s) => s == "",
+            &Message::Plural(ref m) => m.values().all(|s| s == ""),
+        }
+    }
+
+    pub fn singular(&self) -> Option<&str> {
+        match self {
+            &Message::Singular(ref s) => Some(s.as_ref()),
+            _ => None,
+        }
+    }
+}
+
+impl Default for Message {
+    fn default() -> Message { Message::Empty }
+}
+
+/// Note (comment) origins.
+#[derive(Clone,Debug,Eq,PartialEq,Ord,PartialOrd,Hash)]
+pub enum Origin {
+    /// Comment from developer.
+    Developer,
+    /// Comment from translator.
+    Translator,
+    /// Comment with explicit autor or context tag.
+    Tag(String),
+}
+
+/// Translation state.
+///
+/// Indicates whether the translation is considered usable.
+///
+/// # TODO
+///
+/// - Obsolete, NeedsWork, Rejected, Unreviewed (from TT), possibly more.
+#[derive(Copy,Clone,PartialEq,Eq,PartialOrd,Ord,Debug,Hash)]
+pub enum State {
+    /// The unit is not translated.
+    Empty,
+    /// The unit is a suggestion that might be embarrassingly wrong, possibly automatic. It needs
+    /// checking by human translator before it can be used. (Used for `#,fuzzy` entries in `.po`.)
+    NeedsWork,
+    /// The unit is considered usable.
+    Final,
+}
+
+impl Default for State {
+    fn default() -> State { State::Empty }
+}
+
+/// Elementary unit of translation.
+///
+/// A translation unit contains:
+///
+/// - One *source* string, the original message.
+/// - At most one *target* string, the translated message.
+/// - Optional *context* string that disambiguates the original.
+/// - A status. This indicates whether the unit is usable in the software.
+///
+/// # TODO
+///
+/// - Attributes:
+///   - notes (comments)
+///   - locations
+///   - suggestions (alt-trans)
+///   - flags
+///   - …
+/// - Types:
+///   - Plurals (MultiString?)
+///   - Segmentation (xliff 2?)
+#[derive(Clone,Debug,Default)]
+pub struct Unit {
+    _context: Option<String>,
+    _source: Message,
+    _target: Message,
+    _prev_context: Option<String>,
+    _prev_source: Message,
+    _notes: Vec<(Origin, String)>,
+    _locations: Vec<String>,
+    _state: State,
+    _obsolete: bool,
+}
+
+impl Unit {
+    /// Get the context string.
+    pub fn context(&self) -> &Option<String> { &self._context }
+    /// Get the source string.
+    pub fn source(&self) -> &Message { &self._source }
+    /// Get the target string.
+    pub fn target(&self) -> &Message { &self._target }
+    /// Get the previous context (in fuzzy units).
+    pub fn prev_context(&self) -> &Option<String> { &self._prev_context }
+    /// Get the previous source (in fuzzy units).
+    pub fn prev_source(&self) -> &Message { &self._prev_source }
+    /// Get the notes/comments.
+    pub fn notes(&self) -> &Vec<(Origin, String)> { &self._notes }
+    /// Get locations.
+    pub fn locations(&self) -> &Vec<String> { &self._locations }
+    /// Get the state.
+    pub fn state(&self) -> State { self._state }
+    /// Returns whether the unit should be used in application.
+    pub fn is_translated(&self) -> bool { self._state == State::Final }
+    /// Returns whether the unit is obsolete.
+    pub fn is_obsolete(&self) -> bool { self._obsolete }
+}
+
+/// Catalogue reader.
+///
+/// Defines common interface of catalogue readers. Read the units by simply iterating over the
+/// reader. The other methods are for the important metadata.
+pub trait CatalogueReader : Iterator<Item = Result<Unit, Error>> {
+    fn target_language(&self) -> &str;
+    // TODO: More attributes, possibly a generic API
+}
+
+/// Error in reading (and, in future, writing) a catalogue.
+#[derive(Debug)]
+pub enum Error {
+    /// An I/O error from file operation.
+    ///
+    /// The first parameter is line number if applicable, the second is the system error.
+    Io(usize, std::io::Error),
+    /// A parse error.
+    ///
+    /// Parameters are line number, optional unexpected token and an array of expected tokens.
+    /// Unset unexpected token means the parser is not smart enough to remember what it stopped on.
+    /// Empty array of expected items means the parser is not smart enough to remember what it
+    /// could have accepted instead.
+    Parse(usize, Option<String>, Vec<&'static str>),
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            &Error::Io(0, ref err) => err.fmt(f),
+            &Error::Io(line, ref err) => write!(f, "{} at line {}", err, line),
+            &Error::Parse(line, ref got, ref exp) => {
+                write!(f, "Parse error at line {}", line)?;
+                if !exp.is_empty() {
+                    let mut prefix = ", expected";
+                    for e in exp {
+                        write!(f, "{} ‘{}’", prefix, e)?;
+                        prefix = " or";
+                    }
+                }
+                if got.is_some() {
+                    write!(f, ", got ‘{}’", got.as_ref().unwrap())?;
+                }
+                Ok(())
+            }
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn description(&self) -> &str {
+        self.cause().map(std::error::Error::description).unwrap_or("parse error")
+    }
+
+    fn cause(&self) -> Option<&std::error::Error> {
+        match self {
+            &Error::Io(_, ref err) => Some(err),
+            &Error::Parse(..) => None,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
---
+++ src/po.rs
@@ -0,0 +1,348 @@
+//! Handling of [Uniforum Portable Objects][PO]
+//!
+//! This format is used by the well known [gettext] suite and also supported by the
+//! [translate-toolkit][tt] suite. It is a simple text format storing translation units with
+//! optional context and plural variants.
+//!
+//! For modern translation work it's disadvantage is the plural system only supports integers.
+//!
+//! [PO]: https://www.gnu.org/software/gettext/manual/html_node/PO-Files.html
+//! [gettext]: https://www.gnu.org/software/gettext/
+//! [tt]: http://toolkit.translatehouse.org/
+
+use regex::{Regex,Captures};
+use std::collections::{BTreeMap,HashMap};
+use std::io::{BufRead,Lines};
+use std::iter::Peekable;
+use super::{CatalogueReader,Count,Error,Message,Origin,State,Unit};
+
+#[derive(Clone,Debug)]
+enum PoLine {
+    // (number, kind (translator is space), content of the comment)
+    Comment(usize, char, String),
+    // (number, obsolete/previous flag, tag, string)
+    Message(usize, String, String, String),
+    // (number, obsolete/previous flag, string)
+    Continuation(usize, String, String),
+    // ()
+    Blank,
+}
+
+struct LineIter<R: BufRead> {
+    _n: usize,
+    _inner: Lines<R>,
+}
+
+lazy_static!{
+    static ref MESSAGE_RE: Regex = Regex::new(
+        r#"^\s*(#~?\|?)?\s*(msgctxt|msgid|msgif_plural|msgstr(?:\[[012345]\])?)?\s*"(.*)"\s*$"#)
+        .unwrap();
+    static ref COMMENT_RE: Regex = Regex::new(
+        r#"^\s*#([:.,]?)\s*(.*)"#).unwrap();
+
+    static ref UNESCAPE_RE: Regex = Regex::new("\\\\[rtn\"\\]").unwrap();
+    static ref UNESCAPE_MAP: HashMap<&'static str, &'static str> = [
+        (r"\r", "\r"),
+        (r"\t", "\t"),
+        (r"\n", "\n"),
+        ("\\\"", "\""),
+        (r"\\", r"\"),
+    ].iter().cloned().collect();
+}
+
+fn parse_po_line(line: &str, n: usize) -> Result<PoLine, ()> {
+    if !line.contains(|c: char| !c.is_whitespace()) {
+        return Ok(PoLine::Blank);
+    }
+    if let Some(c) = MESSAGE_RE.captures(line) {
+        if c.get(2).is_some() {
+            return Ok(PoLine::Message(
+                    n,
+                    c.get(1).unwrap().as_str().to_owned(),
+                    if c.get(1).unwrap().as_str().ends_with('|') {
+                        String::from("|") + c.get(2).unwrap().as_str()
+                    } else {
+                        c.get(2).unwrap().as_str().to_owned()
+                    },
+                    UNESCAPE_RE.replace(
+                        c.get(3).unwrap().as_str(),
+                        |d: &Captures| -> String {
+                            UNESCAPE_MAP.get(d.get(0).unwrap().as_str()).unwrap().to_string()
+                        }).into_owned()));
+        } else {
+            return Ok(PoLine::Continuation(
+                    n,
+                    c.get(1).unwrap().as_str().to_owned(),
+                    UNESCAPE_RE.replace(
+                        c.get(3).unwrap().as_str(),
+                        |d: &Captures| -> String {
+                            UNESCAPE_MAP.get(d.get(0).unwrap().as_str()).unwrap().to_string()
+                        }).into_owned()));
+        }
+    }
+    if let Some(c) = COMMENT_RE.captures(line) {
+        return Ok(PoLine::Comment(
+                n,
+                c.get(1).unwrap().as_str().chars().next().unwrap_or(' '),
+                c.get(2).unwrap().as_str().to_owned()));
+    }
+    return Err(());
+}
+
+impl<R: BufRead> Iterator for LineIter<R> {
+    type Item = Result<PoLine, Error>;
+    fn next(&mut self) -> Option<Result<PoLine, Error>> {
+        loop {
+            let line = match self._inner.next() {
+                Some(Ok(s)) => s,
+                Some(Err(e)) => return Some(Err(Error::Io(self._n + 1, e))),
+                None => return None,
+            };
+            self._n += 1;
+            match parse_po_line(&line, self._n) {
+                Ok(PoLine::Blank) => (),
+                Ok(p) => return Some(Ok(p)),
+                Err(_) => return Some(Err(Error::Parse(self._n, Some(line), Vec::new()))),
+            }
+        }
+    }
+}
+
+impl<R: BufRead> LineIter<R> {
+    fn new(r: R) -> LineIter<R> {
+        LineIter {
+            _n: 0,
+            _inner: r.lines(),
+        }
+    }
+}
+
+trait MsgParser {
+    fn parse_comments(&mut self, unit: &mut Unit);
+    fn parse_msg(&mut self, tag: &str, unit: &mut Unit) -> Result<Option<String>, Error>;
+    fn expected(&mut self, exp: Vec<&'static str>) -> Result<Option<Unit>, Error>;
+}
+
+impl<R: BufRead> MsgParser for Peekable<LineIter<R>> {
+    fn parse_comments(&mut self, unit: &mut Unit) {
+        while let Some(&Ok(PoLine::Comment(..))) = self.peek() {
+            match self.next() {
+                Some(Ok(PoLine::Comment(_, ',', s))) => {
+                    for flag in s.split(',').map(str::trim) {
+                        match flag {
+                            "fuzzy" => unit._state = State::NeedsWork,
+                            _ => (), // TODO: Implement other flags (do we need any?)
+                        }
+                    }
+                }
+                Some(Ok(PoLine::Comment(_, ':', s))) => {
+                    unit._locations.extend(s.split(",").map(str::trim).map(From::from));
+                }
+                Some(Ok(PoLine::Comment(_, '.', s))) => {
+                    unit._notes.push((Origin::Developer, s));
+                }
+                Some(Ok(PoLine::Comment(_, ' ', s))) => {
+                    unit._notes.push((Origin::Translator, s));
+                }
+                _ => unreachable!(), // we *know* it is a Some(Ok(Comment))
+            }
+        }
+    }
+
+    fn parse_msg(&mut self, tag: &str, unit: &mut Unit) -> Result<Option<String>, Error> {
+        if is!(self.peek() => Some(&Err(_))) {
+            // Get error out of the way
+            return Err(self.next().unwrap().unwrap_err())
+        }
+        
+        let prefix;
+        let mut string;
+
+        if is!(self.peek() =>
+               Some(&Ok(PoLine::Message(_, ref p, ref t, _)))
+               if t == tag && p.starts_with("#~") == unit._obsolete) {
+            if let PoLine::Message(_, p, _, s) = self.next().unwrap().unwrap() {
+                prefix = p;
+                string = s;
+            } else {
+                unreachable!()
+            }
+        } else {
+            return Ok(None); // Not the expected message
+        }
+
+        loop {
+            if is!(self.peek() => Some(&Err(_))) {
+                // Get error out of the way
+                return Err(self.next().unwrap().unwrap_err())
+            }
+
+            if is!(self.peek() =>
+                   Some(&Ok(PoLine::Continuation(_, ref p, _)))
+                   if *p == prefix) {
+                if let PoLine::Continuation(_, _, s) = self.next().unwrap().unwrap() {
+                    string.push_str(&s);
+                } else {
+                    unreachable!();
+                }
+            } else {
+                break;
+            }
+        }
+        Ok(Some(string))
+    }
+
+    fn expected(&mut self, exp: Vec<&'static str>) -> Result<Option<Unit>, Error> {
+        match self.peek() {
+            Some(&Ok(PoLine::Message(n, ref p, ..))) =>
+                Err(Error::Parse(n, Some(p.clone()), exp)),
+            Some(&Ok(PoLine::Continuation(n, ..))) =>
+                Err(Error::Parse(n, Some("\"".to_owned()), exp)),
+            Some(&Ok(PoLine::Comment(n, c, ..))) =>
+                Err(Error::Parse(n, Some(format!("#{}", c)), exp)),
+            None =>
+                Ok(None),
+            _ => panic!("Should not happen!"),
+        }
+    }
+}
+
+fn is_header(oru: &Option<Result<Unit, Error>>) -> bool {
+    match oru {
+        &Some(Ok(ref u)) => u.source().is_singular() && u.source().is_blank(),
+        _ => false,
+    }
+}
+
+pub struct PoReader<R: BufRead> {
+    _lines: Peekable<LineIter<R>>,
+    _next_unit: Option<Result<Unit, Error>>,
+    _failed: Option<Error>,
+    _header: HashMap<String, String>,
+    _target_language: String,
+    _plurals: Vec<Count>,
+}
+
+impl<R: BufRead> PoReader<R> {
+    pub fn new(reader: R) -> Self {
+        let mut res = PoReader {
+            _lines: LineIter::new(reader).peekable(),
+            _next_unit: None,
+            _failed: None,
+            _header: HashMap::new(),
+            _target_language: String::new(),
+            _plurals: Vec::new(),
+        };
+        res._next_unit = res.next_unit();
+        if is_header(&res._next_unit) {
+            res.parse_po_header();
+            res._next_unit = res.next_unit();
+        }
+        return res;
+    }
+
+    fn make_source(msgid: Option<String>, msgid_plural: Option<String>) -> Message {
+        if msgid.is_none() {
+            Message::Empty
+        } else if msgid_plural.is_none() {
+            Message::Singular(msgid.unwrap())
+        } else {
+            let mut map = BTreeMap::new();
+            map.insert(Count::One, msgid.unwrap());
+            map.insert(Count::Other, msgid_plural.unwrap());
+            Message::Plural(map)
+        }
+    }
+
+    fn parse_unit(&mut self) -> Result<Option<Unit>, Error> {
+        let mut unit = Unit::default();
+        self._lines.parse_comments(&mut unit);
+        match self._lines.peek() {
+            None => return Ok(None), // end if no unit (possibly after comments)
+            Some(&Ok(PoLine::Message(_, ref p, ..))) // detect obsolete
+                if p.starts_with("#~") => unit._obsolete = true,
+            _ => (),
+        }
+
+        unit._prev_context = self._lines.parse_msg("|msgctxt", &mut unit)?;
+
+        let prev_msgid = self._lines.parse_msg("|msgid", &mut unit)?;
+        let prev_msgid_pl = if prev_msgid.is_some() {
+            self._lines.parse_msg("|msgid_plural", &mut unit)?
+        } else { None };
+        unit._prev_source = Self::make_source(prev_msgid, prev_msgid_pl);
+
+        unit._context = self._lines.parse_msg("msgctxt", &mut unit)?;
+
+        let msgid = self._lines.parse_msg("msgid", &mut unit)?;
+        if msgid.is_none() {
+            return self._lines.expected(vec!["msgid"]);
+        }
+        let msgid_pl = self._lines.parse_msg("msgid_plural", &mut unit)?;
+        unit._source = Self::make_source(msgid, msgid_pl);
+
+        if unit._source.is_singular() {
+            // sinngular source, so expecting singular target:
+            match self._lines.parse_msg("msgstr", &mut unit)? {
+                None => return self._lines.expected(vec!["msgstr"]),
+                Some(s) => unit._target = Message::Singular(s),
+            }
+        } else {
+            assert!(unit._source.is_plural());
+            const TAGS: &[&'static str] =
+                &["msgstr[0]", "msgstr[1]", "msgstr[2]", "msgstr[3]", "msgstr[4]", "msgstr[5]", "msgstr[6]"];
+            let mut map = BTreeMap::new();
+            for (c, t) in self._plurals.iter().zip(TAGS) {
+                match self._lines.parse_msg(t, &mut unit)? {
+                    None => return self._lines.expected(vec![t]),
+                    Some(s) => { map.insert(*c, s); }
+                }
+            }
+            unit._target = Message::Plural(map);
+        }
+
+        assert!(!unit._source.is_empty());
+        return Ok(Some(unit));
+    }
+
+    fn next_unit(&mut self) -> Option<Result<Unit, Error>> {
+        match self.parse_unit() {
+            Ok(None) => None,
+            Ok(Some(u)) => Some(Ok(u)),
+            Err(e) => Some(Err(e)),
+        }
+    }
+
+    fn parse_po_header(&mut self) {
+        if let Some(Ok(ref u)) = self._next_unit {
+            for line in u._target.singular().unwrap_or("").split('\n') {
+                if let Some(n) = line.find(':') {
+                    let key = line[..n].trim();
+                    let val = line[(n+1)..].trim();
+                    self._header.insert(key.to_owned(), val.to_owned());
+                }
+            }
+            // FIXME FIXME: Extract language
+            // FIXME FIXME: Extract plurals
+        }
+    }
+}
+
+impl<R: BufRead> Iterator for PoReader<R> {
+    type Item = Result<Unit, Error>;
+    fn next(&mut self) -> Option<Result<Unit, Error>> {
+        if self._next_unit.is_none() {
+            return None;
+        }
+
+        let mut res = self.next_unit();
+        ::std::mem::swap(&mut res, &mut self._next_unit);
+        return res;
+    }
+}
+
+impl<R: BufRead> CatalogueReader for PoReader<R> {
+    fn target_language(&self) -> &str {
+        &self._target_language
+    }
+}
